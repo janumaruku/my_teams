ba791fe7e9b65db2919f765ffe8656fbbafe01a8
Liam <liam.harfouche@epitech.eu>
2026-04-08 15:36:50 +0200

    feat(shell): run method

--- src/Client/Shell.cpp
+++ src/Client/Shell.cpp
@@ -9,14 +9,27 @@
 #include <memory>
 #include "Shell.hpp"
 #include "ShellExit.hpp"
+#include "ShellExitException.hpp"
+#include "ShellCommandException.hpp"
 #include "Client.hpp"
 
 namespace my_teams::client {
+
+// using base parameters would be better to avoid duplication but the compiler refuses to move constexpr, which is normal
 
 Shell::Shell() :
     _name(BASE_NAME),
     _prompt(BASE_PROMPT),
     _client(nullptr)
+{
+    _shellCommandFactory.registerCreator<shell::ShellExit>(EXIT_PROMPT);
+}
+
+Shell::Shell(std::string name, std::string prompt) :
+     _name(std::move(name)),
+    _prompt(std::move(prompt)),
+    _client(nullptr)
+
 {
     _shellCommandFactory.registerCreator<shell::ShellExit>(EXIT_PROMPT);
 }
@@ -29,4 +42,26 @@ Shell::Shell(const Client &client, std::string name, std::string prompt) :
     _shellCommandFactory.registerCreator<shell::ShellExit>(EXIT_PROMPT);
 }
     
+void Shell::run()
+{
+    std::string line;
+
+    while (true) {
+        std::cout << _prompt;
+        if (!std::getline(std::cin, line)) {
+            std::cout.flush();
+            return;
+        }
+        if (ShellUtils::isEmptyLine(line))
+            continue;
+        std::vector<std::string> cmd = ShellUtils::split(line, ' ');
+        try {
+            executeCommand(cmd);
+        } catch (const shell::ShellExitException) {
+            return;
+        } catch (const shell::ShellCommandException) {
+            throw;
+        }
+    }
+}
 }
