c1137cc0cac78328ed514895b3c4f23aca415a12
Liam <liam.harfouche@epitech.eu>
2026-04-15 19:14:31 +0200

    chore: logging C++ header

---
+++ src/Client/include/LoggingClient.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2026
+** NTWK
+** File description:
+** 
+*/
+
+#ifndef MYTEAMS_LOGGING_CLIENT_HPP
+#define MYTEAMS_LOGGING_CLIENT_HPP
+
+extern "C" {
+
+#include "logging_client.h"
+
+}
+
+#endif
