c172653d1d5b50af1aeb5b2dc804f8e855c1ecdb
janumaruku <jean-marc.kottin@epitech.eu>
2026-04-15 09:25:43 -0700

    feat(client): TMS-73 #comment Add `HelpCommand` with server request and JSON response handling

---
+++ src/Client/Commands/HelpCommand.cpp
@@ -0,0 +1,49 @@
+/*
+** EPITECH PROJECT, 2026
+** NTWK
+** File description:
+** 
+*/
+
+#include <functional>
+#include <iostream>
+#include "Commands/HelpCommand.hpp"
+#include "Router.hpp"
+#include "Serializer.hpp"
+#include "jsonParser.hpp"
+#include "Client.hpp"
+#include "TeamsShell.hpp"
+
+namespace my_teams::client::shell {
+
+bool HelpCommand::operator()(Shell &shell,
+    std::vector<std::string>)
+{
+    std::cout << "Is helping" << std::endl;
+    nlohmann::json req;
+    req["method"] = network::Method::GET;
+    req["path"] = "/home";
+    req["body"] = {};
+    auto &client = dynamic_cast<TeamsShell &>(shell).getClient();
+
+    client.send(req.dump(), [](auto, auto){});
+    
+    const std::string jsonString = client.receive();
+    Response response = nlohmann::json::parse(jsonString);
+
+    std::cout << response.body.at("message") << std::endl;
+    return true;
+}
+
+bool HelpCommand::execute(Shell &shell,
+    const std::vector<std::string> cmd)
+{
+    return operator ()(shell, cmd);
+}
+
+std::unique_ptr<IShellCommand> HelpCommand::create()
+{
+    return std::unique_ptr<IShellCommand>(new HelpCommand());
+}
+
+}
