e03e0bd42d42456a96b93d9613e3a4a4ee560505
janumaruku <jean-marc.kottin@epitech.eu>
2026-04-15 11:21:19 -0700

    feat(server): TMS-73 #comment Add `ServerLog` header file with `ServerLogger` inclusion

---
+++ src/Server/include/ServerLog.hpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2026
+** my_teams
+** File description:
+** ServerLog
+*/
+
+#ifndef MY_TEAMS_SERVERLOG_HPP
+#define MY_TEAMS_SERVERLOG_HPP
+
+extern "C" {
+    #include "ServerLogger.hpp"
+}
+
+#endif //MY_TEAMS_SERVERLOG_HPP
