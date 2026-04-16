b4693d03411c8c891a411ebebbd96cab1982c520
Liam <liam.harfouche@epitech.eu>
2026-04-16 16:44:15 +0200

    refactor(Commands): List now has lighter syntax

--- src/Client/Commands/ListCommand.cpp
+++ src/Client/Commands/ListCommand.cpp
@@ -50,34 +50,18 @@ bool ListCommand::operator()(Shell &shell,
     client.send(req.dump());
     
     const std::string jsonString = client.receive();
-    Response response = nlohmann::json::parse(jsonString);
-   
+    Response response = nlohmann::json::parse(jsonString); 
 
     switch (context) {
-        case TEAM: {
-            std::vector<Team> teams = response.body;
-            for (const auto &team : teams) {
-                client_print_teams(team.uuid.c_str(), team.name.c_str(), team.description.c_str());
-            }
+        case TEAM:
+            listTeams(response.body);
             break;
-        }
-        case CHANNEL: {
-            std::vector<Channel> channels = response.body;
-            for (const auto &channel : channels) {
-                client_team_print_channels(channel.uuid.c_str(), channel.name.c_str(), channel.description.c_str());
-            }
+        case CHANNEL:
+            listChannels(response.body);
             break;
-        }
-        case THREAD: {
-            std::vector<Thread> threads = response.body;
-            for (const auto &thread : threads) {
-                client_channel_print_threads(thread.uuid.c_str(), thread.userId.c_str(),
-                                            std::chrono::system_clock::to_time_t(thread.createdAt),
-                                            thread.title.c_str(),
-                                            thread.body.c_str());
-            }
+        case THREAD:
+            listThreads(response.body);
             break;
-        }
         default:
             break;
     }
@@ -93,6 +77,47 @@ bool ListCommand::execute(Shell &shell,
 std::unique_ptr<IShellCommand> ListCommand::create()
 {
     return std::unique_ptr<IShellCommand>(new ListCommand());
+}
+
+void ListCommand::logTeam(const Team &team)
+{
+     client_print_teams(team.uuid.c_str(), team.name.c_str(), team.description.c_str());
+
+}
+
+void ListCommand::listTeams(const std::vector<Team> &teams)
+{
+    for (const auto &team : teams)
+        logTeam(team);
+}
+
+void ListCommand::logChannel(const Channel &channel)
+{
+    client_team_print_channels(channel.uuid.c_str(), channel.name.c_str(), channel.description.c_str());
+}
+
+void ListCommand::listChannels(const std::vector<Channel> &channel)
+{
+    for (const auto &channel : channel)
+        logChannel(channel);
+
+}
+
+void ListCommand::logThread(const Thread &thread)
+{
+client_channel_print_threads(
+    thread.uuid.c_str(), thread.userId.c_str(),
+    std::chrono::system_clock::to_time_t(thread.createdAt),
+    thread.title.c_str(),
+    thread.body.c_str());
+
+}
+
+void ListCommand::listThreads(const std::vector<Thread> &threads)
+{
+    for (const auto &thread : threads)
+        logThread(thread);
+
 }
 
 }
