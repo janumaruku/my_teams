4db94a49d734137747541ed2a6bbeab174824ec4
Liam <liam.harfouche@epitech.eu>
2026-04-16 18:01:52 +0200

    refactor(Commands): subscribe

--- src/Client/Commands/SubscribedCommand.cpp
+++ src/Client/Commands/SubscribedCommand.cpp
@@ -10,20 +10,28 @@
 #include "Commands/SubscribedCommand.hpp"
 #include "Router.hpp"
 #include "Serializer.hpp"
-
+#include "Commands/ListCommand.hpp"
 #include "jsonParser.hpp"
 #include "Client.hpp"
 #include "TeamsShell.hpp"
 #include "LoggingClient.hpp"
+#include "logging_client.h"
 
 namespace my_teams::client::shell {
 
 bool SubscribedCommand::operator()(Shell &shell,
     std::vector<std::string> arg)
 {
+    bool list_teams = false;
     nlohmann::json req;
     auto &client = dynamic_cast<TeamsShell &>(shell).getClient();
-    std::string path = arg.empty() ? "/home/users/" + arg.at(1) + "/teams" : "/home/teams/" + arg.at(1) + "/users";
+    std::string path; 
+    if (arg.empty()) {
+        path = "/home/users/" + arg.at(0) + "/teams";
+        list_teams = true;
+    } else {
+        path = "/home/teams/" + arg.at(0) + "/users";
+    }
     req["path"] = path;
     req["method"] = network::Method::GET;
     req["body"] = {};
@@ -38,15 +46,24 @@ bool SubscribedCommand::operator()(Shell &shell,
         return false;
     }
 
-    Team team = response.body;
-    client_print_subscribed(client.getUserId().c_str(), team.uuid.c_str());
+    if (list_teams) {
+        std::vector<Team> teams = response.body;
+        ListCommand::listTeams(teams);
+        return true;
+    }
+    std::vector<User> users = response.body;
+    for (const auto &user : users)
+        client_print_users(user.uuid.c_str(),
+                        user.name.c_str(),
+                        static_cast<int>
+                        (response.statusCode != network::StatusCode::UNAUTHORIZED));
     return true;
 }
 
 bool SubscribedCommand::execute(Shell &shell,
-    const std::vector<std::string> cmd)
+    const std::vector<std::string> args)
 {
-    return operator ()(shell, cmd);
+    return operator ()(shell, args);
 }
 
 std::unique_ptr<IShellCommand> SubscribedCommand::create()
