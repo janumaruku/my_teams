3e51027ee4026e59543377f962eb5eef8c355019
Liam <liam.harfouche@epitech.eu>
2026-04-16 08:57:44 +0200

    feat(Commmands): SendCommand

--- src/Client/Commands/SendCommand.cpp
+++ src/Client/Commands/SendCommand.cpp
@@ -9,6 +9,7 @@
 #include "Commands/SendCommand.hpp"
 #include "Router.hpp"
 #include "Serializer.hpp"
+#include "Types.hpp"
 #include "jsonParser.hpp"
 #include "Client.hpp"
 #include "TeamsShell.hpp"
@@ -16,26 +17,28 @@
 namespace my_teams::client::shell {
 
 bool SendCommand::operator()(Shell &shell,
-    std::vector<std::string>)
+    std::vector<std::string> arg)
 {
-    std::cout << "Is helping" << std::endl;
     nlohmann::json req;
-    req["method"] = network::Method::POST;
-    req["path"] = "/help";
-    req["body"] = {};
     auto &client = dynamic_cast<TeamsShell &>(shell).getClient();
-
+    req["method"] = network::Method::POST;
+    req["path"] = "/home/users/" +  client.getUserId() + "/message";
+    req["body"]["send_to"] = arg.at(0); 
+    req["body"]["message"] = arg.at(1); 
     client.send(req.dump(), [](auto, auto){});
     
     const std::string jsonString = client.receive();
     Response response = nlohmann::json::parse(jsonString);
+    Message message = nlohmann::json::parse(response.body);
+
+    client_event_private_message_received(arg.at(0).c_str(), arg.at(1).c_str());
     return true;
 }
 
 bool SendCommand::execute(Shell &shell,
-    const std::vector<std::string> cmd)
+    const std::vector<std::string> arg)
 {
-    return operator ()(shell, cmd);
+    return operator ()(shell, arg);
 }
 
 std::unique_ptr<IShellCommand> SendCommand::create()
