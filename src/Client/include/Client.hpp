c455f47aa98972fccace0a10c7ddfc98ed2e97cb
Liam <liam.harfouche@epitech.eu>
2026-04-16 04:12:28 +0200

    feat: context definiton7

--- src/Client/include/Client.hpp
+++ src/Client/include/Client.hpp
@@ -8,12 +8,23 @@
 #ifndef MY_TEAMS_CLIENT_HPP
 #define MY_TEAMS_CLIENT_HPP
 
+#include <cstdint>
+#include <map>
 #include "IoContext.hpp"
-#include "stdint.h"
 
 constexpr int EXIT_EPITECH = 84;
 
+constexpr size_t MAX_USE_ARGS = 4;
+
 namespace my_teams::client {
+
+enum CommandContextType : uint8_t {
+        UNDEFINED = 0,
+        USER,
+        TEAM,
+        CHANNEL,
+        THREAD
+};
 
 class Client {
 public:
@@ -24,14 +35,23 @@ public:
 
     std::string receive();
 
-    enum CommandContextType : uint8_t {
-        USER = 0,
-        TEAM,
-        CHANNEL,
-        THREAD
-    };
+    void setContext(const CommandContextType &newContext) noexcept;
+    CommandContextType getContext() const noexcept;
+
+    const std::string &getUserId() const noexcept;
+    void setUserId(std::string &uuid) noexcept;
+
+    const std::string &getTeamId() const noexcept;
+    void setTeamId(std::string &uuid) noexcept;
+
+    const std::string &getChannelId() const noexcept;
+    void setChannelId(std::string &uuid) noexcept;
+
+    const std::string &getThreadId() const noexcept;
+    void setThreadId(std::string &uuid) noexcept;
 
 private:
+
     network::IOContext _ioContext;
     network::ConnectedSocket _socket;
     std::string _buffer;
@@ -40,6 +60,7 @@ private:
     void handleWrite();
 
     void handleRead();
+
     CommandContextType _context;
 
     std::string _userId;
