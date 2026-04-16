37a31cbd1678061d32249d77cdf6c51266bc9ea1
Liam <liam.harfouche@epitech.eu>
2026-04-16 08:57:59 +0200

    feat(Commmands): Subscribe(d)Command

--- src/Client/Commands/SubscribedCommand.cpp
+++ src/Client/Commands/SubscribedCommand.cpp
@@ -5,7 +5,8 @@
 ** 
 */
 
-#include <functional>
+#include <algorithm>
+#include <string>
 #include "Commands/SubscribedCommand.hpp"
 #include "Router.hpp"
 #include "Serializer.hpp"
@@ -13,23 +14,32 @@
 #include "jsonParser.hpp"
 #include "Client.hpp"
 #include "TeamsShell.hpp"
+#include "logging_client.h"
 
 namespace my_teams::client::shell {
 
 bool SubscribedCommand::operator()(Shell &shell,
-    std::vector<std::string>)
+    std::vector<std::string> arg)
 {
-    std::cout << "Is helping" << std::endl;
     nlohmann::json req;
-    req["method"] = network::Method::GET;
-    req["path"] = "/help";
-    req["body"] = {};
     auto &client = dynamic_cast<TeamsShell &>(shell).getClient();
+    std::string path = arg.empty() ? "/home/users/" + arg.at(1) + "/teams" : "/home/teams/" + arg.at(1) + "/users";
+    req["path"] = path;
+    req["method"] = network::Method::GET;
+    req["body"] = {};
 
     client.send(req.dump(), [](auto, auto){});
     
     const std::string jsonString = client.receive();
     Response response = nlohmann::json::parse(jsonString);
+
+    if (response.statusCode != network::StatusCode::STATUS_OK) {
+        std::cout << response.body.at("error_message") << std::endl;
+        return false;
+    }
+
+    Team team = response.body;
+    client_print_subscribed(client.getUserId().c_str(), team.uuid.c_str());
     return true;
 }
 
