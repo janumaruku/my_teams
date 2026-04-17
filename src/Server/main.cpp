a472e850dec1b65541706900f86c029fd094cd77
janumaruku <jean-marc.kottin@epitech.eu>
2026-04-17 08:43:59 -0700

    chore: TMS-14 #comment Reinitialize the main.

--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -10,24 +10,24 @@
 #include "Client.hpp"
 #include "Database.hpp"
 #include "IoContext.hpp"
+#include "Query.hpp"
 #include "Router.hpp"
 #include "Server.hpp"
 
-int main(int /*ac*/, char **/*av*/)
+int main(int ac, char **av)
 {
-    // if (ac < 2)
-    //     return EXIT_EPITECH;
-    //
-    // try {
-    //     const int port = std::stoi(av[1]);
-    //     my_teams::server::Server server{port};
-    //
-    //     server.run();
-    // } catch (const std::exception &e) {
-    //     std::cerr << e.what() << std::endl;
-    //     return EXIT_EPITECH;
-    // }
-    liteORM::Database db{"db"};
+    if (ac < 2)
+        return EXIT_EPITECH;
+
+    try {
+        const int port = std::stoi(av[1]);
+        my_teams::server::Server server{port};
+
+        server.run();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_EPITECH;
+    }
 
     return 0;
 }
