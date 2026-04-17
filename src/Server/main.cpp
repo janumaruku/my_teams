e085d91e3286790036dac0ec94477ef45689dead
janumaruku <jean-marc.kottin@epitech.eu>
2026-04-17 10:16:01 -0700

    feat: server login command

--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -13,31 +13,21 @@
 #include "Query.hpp"
 #include "Router.hpp"
 #include "Server.hpp"
-#include <uuid/uuid.h>
 
-int main(int /*ac*/, char **/*av*/)
+int main(int ac, char **av)
 {
-    // if (ac < 2)
-    //     return EXIT_EPITECH;
-    //
-    // try {
-    //     const int port = std::stoi(av[1]);
-    //     my_teams::server::Server server{port};
-    //
-    //     server.run();
-    // } catch (const std::exception &e) {
-    //     std::cerr << e.what() << std::endl;
-    //     return EXIT_EPITECH;
-    // }
+    if (ac < 2)
+        return EXIT_EPITECH;
 
+    try {
+        const int port = std::stoi(av[1]);
+        my_teams::server::Server server{port};
 
-    uuid_t uuid;
-    char uuid_str[37]; // 36 chars + null terminator
-
-    uuid_generate(uuid);              // generate UUID
-    uuid_unparse(uuid, uuid_str);     // convert to string
-
-    printf("UUID: %s\n", uuid_str);
+        server.run();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_EPITECH;
+    }
 
     return 0;
 }
